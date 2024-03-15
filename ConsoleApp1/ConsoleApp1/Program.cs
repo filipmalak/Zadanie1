74e392c1fbe7548e8d8d3f3180ae3b1e057b9bf7
s28494 <s28494@pjwstk.edu.pl>
2024-03-15 09:19:00 +0100

    "moj pierwszy commit"

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int[] tab = { 1, 2, 4, 4, 43, 2, 4, };
+Console.WriteLine(calc(tab));
+
+static double calc(int[] tab)
+{
+    return tab.Average(); 
+}
